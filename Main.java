df478d1a07ce9f615c43fb5c53eb798db39ddf20
LGordy20 <liam.gordy@patelhs.org>
2021-02-18 18:47:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Ms. Wilson if we all get a good grade on the test can we have a pizza part");
+    scan.next();
+
+
+    public static int averageScore(int a);
+    
+      if(a == 90)
+       System.out.println("");
+      
+  }
+}
