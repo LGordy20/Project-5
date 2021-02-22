c0549e4cdc870c44ca20333809b0e62bcb1d91e8
LGordy20 <liam.gordy@patelhs.org>
2021-02-22 16:27:17 +0000

    Started new method

--- Main.java
+++ Main.java
@@ -3,14 +3,15 @@ class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("Ms. Wilson if we all get a good grade on the test can we have a pizza part");
+    System.out.println("How many students are in this class");
     scan.next();
+    int Students = 20;
 
 
-    public static int averageScore(int a);
+    public static int average(Students);
     
       if(a == 90)
        System.out.println("");
-      
+    
   }
 }
